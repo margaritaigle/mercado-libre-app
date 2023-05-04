bf0908d6aeff1e12962899b7897e1d8b340d5c9b
Margarita Iglesias <margarita.iglesias@clarivate.com>
2023-05-04 17:58:59 -0300

    adding searchbox unit tests

---
+++ src/__tests__/components/SearchBox.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { SearchBox } from "../../components/SearchBox/SearchBox";
+import { vitest } from "vitest";
+import { MemoryRouter, createSearchParams } from "react-router-dom";
+
+const mockedUseNavigate = vitest.fn();
+
+vitest.mock("react-router-dom", () => ({
+  ...require("react-router-dom"),
+  useNavigate: () => mockedUseNavigate,
+  Outlet: () => <></>,
+}));
+
+const params = {q: "Test"}
+describe("Testing SearchBox", () => {
+  test("Should match snapshot", () => {
+    const container = render(
+      <MemoryRouter
+        initialEntries={[
+          { pathname: "/", search: `${createSearchParams(params)}` },
+        ]}
+      >
+        <SearchBox />
+      </MemoryRouter>
+    ).container;
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("should call useNavigate ", () => {
+    render(<SearchBox />);
+    const searchInput = screen.getByRole("textbox");
+    const searchBtn = screen.getByRole("button");
+
+    fireEvent.change(searchInput, { target: { value: "Testing" } });
+    fireEvent.click(searchBtn);
+
+    expect(mockedUseNavigate).toHaveBeenCalled();
+  });
+});
